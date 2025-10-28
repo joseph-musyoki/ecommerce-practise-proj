f799cb112a0bc8cd97133d5dfb4342fe7f193c03
joseph-musyoki <musyokijoseph42@gmail.com>
2025-10-28 09:32:56 -0700

    PathError corrected

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,10 @@ app.use('/api/payments', paymentRouter);
 app.use('/api/analytics', analyticsRouter);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
-    })
+    app.use(express.static(path.join(__dirname, "frontend", "dist")));
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
 }
 
 app.listen(PORT, () =>{
